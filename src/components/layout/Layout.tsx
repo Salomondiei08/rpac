a4e6126850ebac6f516c85a2221b0bcc0a21ee9e
Salomon Diei <salomondiei08@gmail.com>
2025-11-19 16:00:35 +0900

    chore: edited fonts

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,29 +1,19 @@
 import { ReactNode } from "react";
-import { useLocation } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SEO from "./SEO";
 import ScrollToTop from "./ScrollToTop";
-import { cn } from "@/lib/utils";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const location = useLocation();
-  const isHomePage = location.pathname === "/" && location.hash.length === 0;
-
   return (
     <>
       <SEO />
       <ScrollToTop />
-      <div
-        className={cn(
-          "flex min-h-screen flex-col",
-          !isHomePage && "font-brand text-[17px] leading-relaxed"
-        )}
-      >
+      <div className="flex min-h-screen flex-col">
         <Navbar />
         <main id="main" className="flex-1 focus:outline-none pb-24" tabIndex={-1}>
           {children}
