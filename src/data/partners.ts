49d364037cd1937f7548635efa401134ef78b25b
Salomon Diei <salomondiei08@gmail.com>
2026-01-03 15:25:18 +0900

    chore: edited partern name

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -14,7 +14,7 @@ export const partners: Partner[] = [
     url: "https://observatoireagenda2030.uqam.ca/",
   },
   {
-    name: "NIM ADVISORY Mussampa",
+    name: "NIM ADVISORY",
     logo: nmadvisoryLogo,
     url: "https://www.nmadvisory.ca/",
   },
